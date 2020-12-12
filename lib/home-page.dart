533a14c37b2ada1b6116d549cac2015439b14404
SchemeSonic <haldun313@gmail.com>
2020-12-12 22:27:17 +0300

    Update home page

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,4 +1,7 @@
+import 'package:example_app/investment-crud.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'globals.dart' as globals;
 import 'apiclient.dart' as apiclient;
 
@@ -9,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Widget> exchanges = [];
+  Widget investmentSummary;
   var dolarRate, euroRate, goldRate;
   
   getData() async {
@@ -27,23 +31,188 @@ class _HomePageState extends State<HomePage> {
           title: Text(entry['YUKSEK'].toString()),
           subtitle: Text(entry['ACIKLAMA']),
         )).toList();
+
+    currencyWidget.insert(0, Text("Döviz ve Altın Kurları", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),));
+      
     setState(() {
       exchanges = currencyWidget;
     });
+    getInvestments();
   }
 
+  Widget getSummaryContainer(summary) {
+    return Container(
+          padding: EdgeInsets.fromLTRB(20, 30, 0, 30),
+          height: 200,
+          alignment: Alignment.centerLeft,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Toplam Varlık", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54)),
+                      Text("${summary['grandTotal'].toStringAsFixed(0)} TL", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 40))
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Text("${summary['rate'].toStringAsFixed(1)}%", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green, fontSize: 40)),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft:  Radius.circular(40), bottomLeft:  Radius.circular(40)),
+                    )
+                  )
+                ]
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Varlık Ayrıntıları", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          FaIcon(FontAwesomeIcons.dollarSign, size: 36.0, color: Colors.white,),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:
+                              [
+                                Text("Dolar", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54)),
+                                Text("${summary['totalDolar'].toStringAsFixed(0)}", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))
+                              ]
+                            )
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          FaIcon(FontAwesomeIcons.euroSign, size: 36.0, color: Colors.white),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:
+                              [
+                                Text("Euro", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54)),
+                                Text(summary['totalEuro'].toStringAsFixed(0), style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))
+                              ]
+                            )
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          FaIcon(FontAwesomeIcons.coins, size: 36.0, color: Colors.white),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:
+                              [
+                                Text("Altın", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54)),
+                                Text("${summary['totalGold'].toStringAsFixed(2)}gr", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))
+                              ]
+                            )
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ]
+              )
+            ],
+          ),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(50),
+            )
+          ),
+        );
+  }
+
+  getInvestments() async {
+    if (investmentSummary != null)  return;
+    double initialTotal = 0;
+    Map<String, double> totals = {
+      'totalDolar': 0, 
+      'totalEuro': 0, 
+      'totalGold': 0, 
+      'grandTotal': 0,
+      'rate': 1
+    };
+
+    CollectionReference investmentAPI = FirebaseFirestore.instance.collection('Investments');
+    QuerySnapshot snapshot = await investmentAPI.get();
+    for (var doc in snapshot.docs) {
+      var data = doc.data();
+      var investment = new Investment(null, data['currency'], data['date'], data['perPrice'], data['amount']);
+
+      if (investment.currency == 0) {
+        totals['totalDolar'] += investment.amount;
+        totals['grandTotal'] += investment.amount * dolarRate['SATIS'];
+      }
+      if (investment.currency == 1) {
+        totals['totalEuro'] += investment.amount;
+        totals['grandTotal'] += investment.amount * euroRate['SATIS'];
+      }
+      if (investment.currency == 2) {
+        totals['totalGold'] += investment.amount;
+        totals['grandTotal'] += investment.amount * goldRate['SATIS'];
+      }
+
+      initialTotal += investment.amount * investment.perPrice;
+    }
+    
+    totals['rate'] = (totals['grandTotal'] / initialTotal) * 100;
+    print(totals);
+    Widget summary = getSummaryContainer(totals);
+
+    setState(() {
+      investmentSummary = summary;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     getData();
-    return exchanges.length == 0 ? 
+
+    return exchanges.length == 0 || investmentSummary == null ? 
       Center(
         child: CircularProgressIndicator(),
-      ) :
-      Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: exchanges,
-        ),
+      ) : Column(
+        children:[
+          investmentSummary,
+          Flexible(
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: exchanges,
+              ),
+            )
+          ,)
+        ]
       );
   }
 }
